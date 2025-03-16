6c30390f95f52b6e68bd629b17933cbb57a4faf8
donyfs <donyfs@ymail.com>
2025-03-16 14:17:03 +0700

    add reset database before and after running test

--- run-cy-ui-test.sh
+++ run-cy-ui-test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd cypress
 
+echo "Reset database"
+echo "[]" > ../web-sample/users.json
+
 start_time=$(date +%s)
 echo "Test started at: $(date '+%Y-%m-%d %H:%M:%S')"
 
@@ -20,3 +23,6 @@ seconds=$((duration % 60))
 
 echo "Test duration: $duration seconds"
 echo "Test duration: ${hours}h ${minutes}m ${seconds}s"
+
+echo "Reset database"
+echo "[]" > ../web-sample/users.json
